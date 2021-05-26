df7bd0088e5990311a4eb4c42ae20a96a2ae3059
Jhoan David Ramirez Grajales <jhoan.harry@gmail.com>
2021-05-25 22:25:12 -0500

    cardDeck

---
+++ helpers/index.js
@@ -0,0 +1,25 @@
+export function shuffle(cardsDeck){
+    for (let i = cardsDeck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cardsDeck[i], cardsDeck[j]] = [cardsDeck[j], cardsDeck[i]];
+    }
+    return cardsDeck;
+}
+
+export function calculatePoints(playerHand){
+    let aceAdjuts = false,
+    points = 0;
+
+    playerHand.map((card,_index) => {
+      if(card.name == 'A' && !aceAdjuts) {
+        aceAdjuts = true;
+        points = points + 10;
+      }
+      points = points + card.value;
+    });
+
+    if(aceAdjuts && points > 21){
+      points = points - 10;
+    }
+    return points;
+}
